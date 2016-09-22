56dc83ac2310671b3e3b121032243796b8595dc3
Rohit Gangrade <rohitgangrade1@gmail.com>
2016-09-22 06:10:33 +0000

    2nd commit

--- led_blink/hello.py
+++ led_blink/hello.py
@@ -2,15 +2,22 @@ import time
 import RPi.GPIO as GPIO
 
 
-LED = 7
+LED = 16
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(LED, GPIO.OUT)
 GPIO.output(LED, GPIO.LOW)
+GPIO.setup(13, GPIO.OUT)
+GPIO.output(13, GPIO.LOW)
+GPIO.setup(11, GPIO.OUT)
+GPIO.output(11, GPIO.LOW)
+
 
 while True:
-    GPIO.output(LED, GPIO.HIGH)
-    time.sleep(0.5)
     GPIO.output(LED, GPIO.LOW)
+    GPIO.output(13, GPIO.LOW)
+    GPIO.output(11, GPIO.LOW)
+    time.sleep(0.5)
+    #GPIO.output(LED, GPIO.LOW)
     time.sleep(0.5)
 
 
