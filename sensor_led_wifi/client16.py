59779b43357688f1c5eeb8938b56de58612999e7
Rohit Gangrade <rohitgangrade1@gmail.com>
2016-10-16 00:59:37 +0000

    Adding comments to print

--- sensor_led_wifi/client16.py
+++ sensor_led_wifi/client16.py
@@ -16,24 +16,24 @@ sleep_time = 0.1
 while True:
     time.sleep(sleep_time)
     if (GPIO.input(SENSOR)):
-        print('Pin is high')
+        ####print('Pin is high')
         try:
 	    # Connect the socket to the port where the server is listening
 	    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	    server_address = ('192.168.1.6', 10000)
-	    print >>sys.stderr, 'connecting to %s port %s' % server_address
+	    ####print >>sys.stderr, 'connecting to %s port %s' % server_address
 	    sock.connect(server_address)
 
             # Send data
             message = '16'
-            print >>sys.stderr, 'sending "%s"' % message
+            ####print >>sys.stderr, 'sending "%s"' % message
             sock.sendall(message)
         finally:
-            print >>sys.stderr, 'closing socket'
+            ####print >>sys.stderr, 'closing socket'
             sock.close()
     	time.sleep(2.0)
     else:
-        print('Pin is low')
+        ####print('Pin is low')
 
 
 
